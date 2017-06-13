1fa1cbe897d423fb72432895e86d55f8c859505e
Chiraag Prafullchandra <cprafull@ucsd.edu>
2017-06-12 20:01:14 -0700

    Revert "tried to add twilio and gmail notifications?"
    This reverts commit da56aa1f42fabd0e141d895805f0d409c9cf5b98.

--- server/test/notification.js
+++ server/test/notification.js
@@ -9,34 +9,20 @@ var Email = require('../notification/email');
 var TextModel = require('../notification/text');
 
 // SAMPLE : [{phone_number: "XXX-XXX-XXXX", email: "XXXXX@XXXXX.com"}];
-var employees = [{phone_number: "555-555-5555", email:"kissmyapp2017@gmail.com"}];
-var employee_fail = [];
+var employees = [];
 
 describe("Notification", function() {
 
     it('It should send an email', function(done){
       this.timeout(9000);
       Email.sendEmail("Tony Montana", employees, done);
-      done();
+      //done();
     });
 
     it('It should send an text', function(done){
       this.timeout(9000);
       TextModel.sendText("Tony Montana", employees, done);
-      done();
-    });
-
-   it('It should not send an email', function(done){
-      this.timeout(9000);
-      Email.sendEmail("Tony Montana", employee_fail, done);
       //done();
     });
-
-    it('It should not send an text', function(done){
-      this.timeout(9000);
-      TextModel.sendText("Tony Montana", employee_fail, done);
-      //done();
-    });
-
   }
 );
