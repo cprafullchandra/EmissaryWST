fbe235eed0a6d62b240893f2b797dd230a8fe520
Chiraag Prafullchandra <cprafull@ucsd.edu>
2017-06-15 06:21:30 -0700

    moved all images into one location; finished gulp rework; gulp now minifes images; unable to minify all js due to es6 incompatibilities; updated package.json

--- gulpfile.js
+++ gulpfile.js
@@ -7,97 +7,166 @@ let gulp = require('gulp'),
     uglifyJS = require('gulp-uglify');
 
 let paths = {
-  html: 'client/assets/views/*.html',
-  images: 'client/assets/images/*.{JPG,jpg,png,gif,ico}',
-  css: 'client/**/*.css',
-  font: 'client/assets/css/font-icons/entypo/font/*',
-  js: 'client/**/*.js'
+  css: {
+    all: 'client/css/**/*.css',
+    lib: 'client/css/lib/*.css',
+    native: 'client/css/*.css'
+  },
+  developer_docs: 'developer_docs/*.md',
+  dist: {
+    base: 'dist',
+    css: 'dist/css',
+    fonts: 'dist/fonts',
+    html: 'dist',
+    images: 'dist/images',
+    js: 'dist/js'
+  },
+  fonts: 'client/css/lib/font-icons/**/*.{eot,svg,ttf,woff,woff2,otf}',
+  html: 'client/html/*.html',
+  images: 'client/images/*.{JPG,jpg,png,gif,ico}',
+  js: {
+    all: '**/*.js',
+    client: 'client/js/**/*.js',
+    lib: 'client/js/lib/**/*.js',
+    native: 'client/js/*.js',
+    server: 'server/**/*.js',
+    test: 'test/*.js',
+    util: 'util/*.js'
+  }
 };
 
 /* Remove the generated dist */
 gulp.task('clean', function () {
-  return del('./dist');
+  return del(paths.dist.base);
 });
 
 /* Generate documentation based off code comments */
 gulp.task('doc', function() {
-   gulp.src(['README.md',
-   "./client/assets/native/js/*.js",
-   "./server/*.js",
-   "./server/config/*.js",
-   "./server/models/*.js",
-   "./server/notification/*.js",
-   "./server/routes/*",
-   "./server/socket/*.js",
-   "./server/test/*.js"])
-   .pipe(jsdoc());
+  return gulp.src(['README.md',
+    paths.js.native,
+    paths.js.server,
+    paths.js.test,
+    paths.js.util])
+    .pipe(jsdoc());
 });
 
-/* Copy all client source code over */
-gulp.task('copy', function () {
-  return gulp.src('./client/**')
-    .pipe(gulp.dest('./dist/'));
+/* Copy all css files */
+gulp.task('copy:css', function() {
+  return gulp.src(paths.css.all)
+    .pipe(gulp.dest(paths.dist.css));
 });
 
-/* Concatenate all css files into bundle.css and places it
- * in dist/assets folder
+/* Copy all fonts */
+gulp.task('copy:fonts', function() {
+  return gulp.src(paths.fonts)
+    .pipe(gulp.dest(paths.dist.fonts));
+});
+
+/* Copy all html */
+gulp.task('copy:html', function() {
+  return gulp.src(paths.html)
+    .pipe(gulp.dest(paths.dist.html));
+});
+
+/* Copy all images */
+gulp.task('copy:images', function() {
+  return gulp.src(paths.images)
+    .pipe(gulp.dest(paths.dist.images));
+});
+
+/* Copy all client-side js */
+gulp.task('copy:js', function() {
+  return gulp.src(paths.js.client)
+    .pipe(gulp.dest(paths.dist.js));
+});
+
+/* Copy all client-side source code */
+gulp.task('copy', gulp.parallel('copy:css', 'copy:fonts', 'copy:html', 'copy:images', 'copy:js'));
+
+/* Concatenate some css files into bundle.css and places it
+ * in proper folder
  */
 gulp.task('concat:css', function() {
   return gulp.src([
-    './client/assets/css/bootstrap.min.css',
-    './client/assets/css/neon.css'
+    paths.dist.css+'/lib/bootstrap.min.css',
+    paths.dist.css+'/lib/neon.css'
     ])
     .pipe(concat('bundle.css'))
-    .pipe(gulp.dest('./dist/assets/css'));
+    .pipe(gulp.dest(paths.dist.css));
 });
 
+/* Concatenate some native files and css files into bundleAppointments.css
+ * and place it into proper folder
+ */
 gulp.task('concat:cssAppointment', function() {
   return gulp.src([
-    './client/assets/css/bootstrap.css',
-    './client/assets/css/neon-core.css',
-    './client/assets/css/neon-theme.css',
-    './client/assets/native/css/shared-dashboard.css'
+    paths.dist.css+'/lib/bootstrap.css',
+    paths.dist.css+'/lib/neon-core.css',
+    paths.dist.css+'/lib/neon-theme.css',
+    paths.dist.css+'/shared-dashboard.css'
     ])
     .pipe(concat('bundleAppointments.css'))
-    .pipe(gulp.dest('./dist/assets/css'));
+    .pipe(gulp.dest(paths.dist.css));
 });
 
+/* Concatenate some compatible js lib files into bundle.js and
+ * place it into proper folder.
+ */
 gulp.task('concat:js', function() {
   return gulp.src([
-    './client/assets/js/bootstrap.js',
-    './client/assets/js/joinable.js',
-    './client/assets/js/neon-custom.js',
-    './client/assets/js/gsap/TweenMax.min.js',
-    './client/assets/js/js.cookie.js',
-    './client/assets/js/socket.io.js',
-    './client/assets/js/neon-api.js',
-    './client/assets/js/handlebars-v4.0.5.js',
-    './client/assets/js/neon-slider.js',
-    './client/assets/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js'
+    paths.dist.js+'/lib/bootstrap.js',
+    paths.dist.js+'/lib/joinable.js',
+    paths.dist.js+'/lib/neon-custom.js',
+    paths.dist.js+'/lib/gsap/TweenMax.min.js',
+    paths.dist.js+'/lib/js.cookie.js',
+    paths.dist.js+'/lib/socket.io.js',
+    paths.dist.js+'/lib/neon-api.js',
+    paths.dist.js+'/lib/handlebars-v4.0.5.js',
+    paths.dist.js+'/lib/neon-slider.js',
+    paths.dist.js+'/lib/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js'
   ])
   .pipe(concat('bundle.js'))
-  .pipe(gulp.dest('./dist/assets/js'));
+  .pipe(gulp.dest(paths.dist.js));
 });
 
 /* Concat all compatible css and js files together */
 gulp.task('concat', gulp.parallel('concat:css', 'concat:cssAppointment', 'concat:js'));
 
-/* Minifes the css to bundleAppointments.css */
+/* Minifies css */
 gulp.task('minify:css', function() {
-  return gulp.src('./dist/assets/css/bundleAppointments.css')
+  return gulp.src(paths.dist.css+'/**/*.css')
     .pipe(cleanCSS({level: 0, compatibility: '*'}))
-    .pipe(gulp.dest('./dist/assets/css'));
+    .pipe(gulp.dest(paths.dist.css));
 });
 
-/* Minifies the javascript to bundle.js */
+/* Minifies images */
+gulp.task('minify:images', function() {
+  return gulp.src(paths.dist.images+'/*')
+    .pipe(minifyImg())
+    .pipe(gulp.dest(paths.dist.images));
+});
+
+/* Minifies js */
+/* This function should handle all minification of js. Due to
+ * issues with gulp-uglify and ES6 minification, we are not able
+ * to use this yet. However, should this code be inherited, try
+ * commenting out this function to see if it works for you. :)
+ * Note* - Make sure to update your gulp-uglify package first.
+ */
+// gulp.task('minify:js', function() {
+//   return gulp.src(paths.dist.js+'/**/*.js')
+//     .pipe(uglifyJS())
+//     .pipe(gulp.dest(paths.dist.js));
+// });
+
 gulp.task('minify:js', function() {
-  return gulp.src('./dist/assets/js/bundle.js')
+  return gulp.src(paths.dist.js+'/bundle.js')
     .pipe(uglifyJS())
-    .pipe(gulp.dest('./dist/assets/js'));
+    .pipe(gulp.dest(paths.dist.js));
 });
 
 /* Minify css and js */
-gulp.task('minify', gulp.parallel('minify:css', 'minify:js'));
+gulp.task('minify', gulp.parallel('minify:css', 'minify:images', 'minify:js'));
 
 /* Build the app without minification
  * First clean is called to remove any stale code
